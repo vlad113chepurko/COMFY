20fbed23928d82568eaffefe2bf9ef8f681d7a1b
Ліна Чепурко <vlad113chepurko@gmail.com>
2025-11-27 09:17:53 +0200

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/src/layout/Header/Header";
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Welcome to Comfy App</h1>
+      </main>
+    </>
+  );
+}
