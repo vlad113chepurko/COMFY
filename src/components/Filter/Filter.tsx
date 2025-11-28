b54666c491cdb3856ae97485065fb90500990688
Ліна Чепурко <vlad113chepurko@gmail.com>
2025-11-28 14:23:42 +0200

    added filter

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -1,5 +1,38 @@
+"use client";
 import "./Filter.css";
+import { useState } from "react";
 
 export default function Filter() {
-  return <div className="filter"></div>;
+  const [price, setPrice] = useState({ min: 8, max: 3000 });
+
+  const handleMinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice((prev) => ({ ...prev, min: Number(e.target.value) }));
+  };
+
+  const handleMaxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPrice((prev) => ({ ...prev, max: Number(e.target.value) }));
+  };
+
+  return (
+    <div className="filter">
+      <section className="flex-section">
+        <label htmlFor="price">Ціна</label>
+        <div className="flex flex-row justify-center align-center gap-1">
+          <input
+            name="minPrice"
+            type="number"
+            value={price.min}
+            onChange={handleMinChange}
+          />
+          -
+          <input
+            name="maxPrice"
+            type="number"
+            value={price.max}
+            onChange={handleMaxChange}
+          />
+        </div>
+      </section>
+    </div>
+  );
 }
